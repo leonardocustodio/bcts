23c13e9104937248b70dc951044dc5b84b295d41
Leonardo Custodio <leonardo@custodio.me>
2025-12-29 23:44:45 +0700

    package and core types

---
+++ packages/envelope-pattern/src/index.ts
@@ -0,0 +1,28 @@
+/**
+ * @bcts/envelope-pattern - Pattern matching for Gordian Envelope structures
+ *
+ * This is a 1:1 TypeScript port of bc-envelope-pattern-rust.
+ *
+ * @module envelope-pattern
+ */
+
+// Re-export types from dcbor-pattern that are used in this package
+export { Interval, Quantifier, Reluctance } from "@bcts/dcbor-pattern";
+
+// TODO: Implement error types
+// export * from "./error";
+
+// TODO: Implement format utilities
+// export * from "./format";
+
+// TODO: Implement pattern types
+// export * from "./pattern";
+
+// TODO: Implement parsing
+// export * from "./parse";
+
+/**
+ * Placeholder - package implementation pending.
+ * See PLAN.md for implementation details.
+ */
+export const VERSION = "1.0.0-alpha.11";
